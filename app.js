ee9c52cd8392d90fcdc11ee920eef1132348c21e
MegaVital <kalist.fl@gmail.com>
2022-08-15 15:27:50 +0300

    registration

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const app = express();
 
 // Settings
 
-app.set('port', 1213)
+app.set('port', 1111)
 app.set('json spaces', 4)
-
 // middleware
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+
+    // Pass to next layer of middleware
+    next();
+});
 // routes
 
 app.use(Routes)
--- routes/index.js
+++ routes/index.js
@@ -2,42 +2,91 @@ import { Router } from 'express'
 import { _data } from '../_mock_data.js'
 import { JsonDB } from 'node-json-db';
 import { Config } from 'node-json-db/dist/lib/JsonDBConfig.js'
+import JWT from 'jsonwebtoken'
+import crypto from 'crypto'
 
-const db = new JsonDB(new Config("ReactPtojectDB", true, false, '/'));
+const db = new JsonDB(new Config("ReactProjectDB", true, true, '/'));
 const router = Router()
 
-router.get('/', async (req, res) => {
-    await db.push(
-        "/users",
-        [{
-            login: 'Test',
-            pass: '$Hash'
-        }],
-        true
-        );
-    const data = await db.getData("/");
+
+
+router.post('/registration', async (request, response) => {
     try {
+        response.contentType('application/json')
+        const isDBPathExists = await db.exists('/users')
+        if (isDBPathExists) {
+            const matchedUser = await db.find('/users', (entry) => {
+                return entry.email === request.body.email
+            })
+            if (matchedUser) {
+                response.status(405)
+                return response.json({ success: false })
+            }
+        }
+        const newUser = { ...request.body, id: crypto.randomBytes(8).toString('hex') }
+        await db.push(
+            "/users",
+            [newUser],
+            false
+        );
+        delete newUser.password
+        delete newUser.email
+        const token = JWT.sign(newUser, 'secretKey1', {
+            expiresIn: "2 days"
+        })
+        return response.json({ success: true, token })
+    }
+    catch (error) {
+        console.log(error)
+        return response.status(500).json({ success: false, error })
+    }
+
+})
+
+router.get('/regist', async (req, res) => {
+    try {
+
+        const data = await db.getData("/regist");
         res.contentType('application/json')
         res.json(data)
         return res
     }
-    catch(error) {
+    catch (error) {
         console.log(error)
-        return res.status(500).json({error})
+        return res.status(500).json({ error })
     }
-    
 })
-
 router.get('/data', async (req, res) => {
     try {
+
+        const data = await db.getData("/data");
         res.contentType('application/json')
-        res.json(_data)
+        res.json(data)
         return res
     }
-    catch(error) {
+    catch (error) {
         console.log(error)
-        return res.status(500).json({error})
+        return res.status(500).json({ error })
     }
 })
 
 export default router
+
+// router.get('/users', async (req, res) => {
+//     const data = await db.getData(
+//         'users'
+//     )
+
+
+//     try {
+//         res.contentType('application/json')
+//         res.json(data)
+
+//         return res
+//     }
+//     catch (error) {
+//         console.log(error)
+//         return res.status(500).json({ error })
+//     }
+
+// })
