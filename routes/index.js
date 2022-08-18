bc91a4c60b1bf4f29ed896e36b08111da7bd3b7b
MopIIex <mopiiex20@gmail.com>
2022-08-18 10:53:47 +0200

    login

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,41 @@ import { JsonDB } from 'node-json-db';
 import { Config } from 'node-json-db/dist/lib/JsonDBConfig.js'
 import JWT from 'jsonwebtoken'
 import crypto from 'crypto'
+import bcrypt from 'bcryptjs'
 
 const db = new JsonDB(new Config("ReactProjectDB", true, true, '/'));
 const router = Router()
 
+const GoogleSignIn = async ( body)  => {
+    try {
+        const isDBPathExists = await db.exists('/users')
+        if (isDBPathExists) {
+            const matchedUser = await db.find('/users', (entry) => {
+                return entry.google_id === body.google_id
+            })
+            if(matchedUser) {
+                return matchedUser
+            }
+        }
 
+        const newUser = { ...body, id: crypto.randomBytes(8).toString('hex') }
+        await db.push(
+            "/users",
+            [newUser],
+            false
+        );
+      const payload = {
+        name : newUser.name,
+        id : newUser.id
+      }
+
+        return payload
+    }
+    catch (error) {
+        return error
+    }
+
+}
 
 router.post('/registration', async (request, response) => {
     try {
@@ -16,11 +46,11 @@ router.post('/registration', async (request, response) => {
         const isDBPathExists = await db.exists('/users')
         if (isDBPathExists) {
             const matchedUser = await db.find('/users', (entry) => {
-                return entry.email === request.body.email || entry.name === request.body.name
+                return entry.email === request.body.email
             })
             if (matchedUser) {
                 response.status(405)
-                return response.json({ success: false })
+                return response.json({ success: false , error : { message : 'User with such email alreadt exists!'}})
             }
         }
         const newUser = { ...request.body, id: crypto.randomBytes(8).toString('hex') }
@@ -38,6 +68,48 @@ router.post('/registration', async (request, response) => {
     }
     catch (error) {
         console.log(error)
+        return response.status(500).json({ success: false, error })
+    }
+
+})
+
+router.post('/login', async (request, response) => {
+    if(!request.body.login || !request.body.password  ) {
+        return response.status(403).json({ success: false , error : {message :  'Credentials are not provided'}})
+    }
+    try {
+        response.contentType('application/json')
+        const isDBPathExists = await db.exists('/users')
+        if (!isDBPathExists) {
+            return response.status(402).json({ success: false , error : {message :  'No user records found!'}})
+        }
+
+        const matchedUser = await db.find('/users', (entry) => {
+            return entry.email === request.body.email
+        })
+
+        if (!matchedUser) {
+            response.status(403)
+            return response.json({ success: false , error : { message : 'Password or email invalid'}})
+        }
+
+        const passwordCheck = await  bcrypt.compare(request.body.password, matchedUser.password)
+        if (!passwordCheck) {
+            response.status(403)
+            return response.json({ success: false , error : { message : 'Password or email invalid'}})
+        }
+
+      const payload = {
+        name : matchedUser.name,
+        id : matchedUser.id
+      }
+
+        const token = JWT.sign(payload, 'secretKey1', {
+            expiresIn: "2 days"
+        })
+        return response.json({ success: true, token })
+    }
+    catch (error) {
         return response.status(500).json({ success: false, error })
     }
 
